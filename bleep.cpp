c34bec2e44b8a06be94ae30126b6318afe429664
JadeyTuff <155403590+JadeyTuff@users.noreply.github.com>
2024-01-01 17:51:55 -0800

    Create bleep.cpp

---
+++ bleep.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "functions.hpp"
+
+int main() {
+
+  std::string word = "broccoli";
+  std::string text = "Barclay sounds like broccoli and calling someone broccoli is funny";
+
+  std::cout << bleep(word, text) << "\n";
+
+}
